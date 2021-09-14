b14544542822aa62653781757719d68098e03c38
hien123454 <ngochien15092000@gmail.com>
2021-09-14 17:47:10 +0700

    repo lan 1

--- src/main/java/com/example/TranNgocHien18049511CRUDSpringDataJPA/repository/CustomerRepository.java
+++ src/main/java/com/example/TranNgocHien18049511CRUDSpringDataJPA/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.example.TranNgocHien18049511CRUDSpringDataJPA.repository;
 import com.example.TranNgocHien18049511CRUDSpringDataJPA.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    List<Customer> findByName(String name);
 }
